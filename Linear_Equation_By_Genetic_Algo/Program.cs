9f5288fe3fba83c50625f87e87d24ab445d6501b
M Ghous Sarwar <ghoussarwar@gmail.com>
2018-03-23 21:32:58 +0500

    Initial

---
+++ Linear_Equation_By_Genetic_Algo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Equation_By_Genetic_Algo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
